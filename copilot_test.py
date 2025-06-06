4d3e2fcc762d5aa02597d87b079c9a23c7c7a30d
Radhika mathur <radhika.official02@gmail.com>
2025-06-06 11:19:58 +0530

    copilot_test.py

--- copilot_test.py
+++ copilot_test.py
@@ -1,1 +1,15 @@
+import os
+import time
 
+def get_uptime():
+    # For Linux systems
+    if os.name == "posix":
+        with open('/proc/uptime', 'r') as f:
+            uptime_seconds = float(f.readline().split()[0])
+            uptime_str = time.strftime('%H:%M:%S', time.gmtime(uptime_seconds))
+            print(f"System Uptime: {uptime_str} (HH:MM:SS)")
+    else:
+        print("This script currently supports Linux systems only.")
+
+if __name__ == "__main__":
+    get_uptime()
